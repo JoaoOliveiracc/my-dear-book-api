f8f022d58d6bccf1f4051536a1cb84b601890587
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-06-03 14:44:29 -0300

    Search, register, edition

--- src/app.js
+++ src/app.js
@@ -1,9 +1,47 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const books = [
+    {
+        id: 1,
+        title: "O Senhor dos Anéis" 
+    },
+    {
+        id: 2,
+        title: "O Corvo" 
+    }
+];
+
+function searchBook(id) {
+    return books.findIndex(book => {
+        return book.id === Number(id)
+    });
+}
 
 app.get("/", (req, res) => {
     res.status(200).send("My dear book");
+});
+
+app.get("/books", (req, res) => {
+    res.status(200).json(books);
+});
+
+app.post("/books",  (req, res) => {
+    books.push(req.body);
+    res.status(201).send("Registered book");
+});
+
+app.get("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    res.status(200).json(books[index]);
+});
+
+app.put("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books);
 });
 
 export default app;
